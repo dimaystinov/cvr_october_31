a0934549a53df06892772293710e1f564d04c600
dimaystinov <50107426+dimaystinov@users.noreply.github.com>
2024-10-31 14:17:45 +0300

    Add files via upload

---
+++ 1_camera.py
@@ -0,0 +1,20 @@
+import cv2
+from pprint import pprint
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    # ret - получилось ли достать кадр return code
+    # frame - сам кадр
+    ret, frame = cap.read()
+    print(ret)
+    for el in frame:
+        for i in el:
+            print(i,end='')
+        print()
+
+    # Название окна , сам кадр
+    cv2.imshow('camera', frame)
+    # Ожидать нажатия клавиши 1 мс
+    cv2.waitKey(1)
+    break
